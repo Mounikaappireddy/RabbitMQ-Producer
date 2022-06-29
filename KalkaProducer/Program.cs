a9864c238624232e424012d95331c2ee5a0766ec
unknown <cogdotnet902@iiht.tech>
2022-06-29 15:33:05 +0530

    rabbitmq

---
+++ KalkaProducer/Program.cs
@@ -0,0 +1,20 @@
+﻿using RabbitMQ.Client;
+using System;
+
+namespace KalkaProducer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var Factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672")
+            };
+            var connection = Factory.CreateConnection();
+            var channel = connection.CreateModel();
+            QueueProducer.Publish(channel);
+    } }
+}
---
+++ KalkaProducer/QueueProducer.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KalkaProducer
+{
+   public static class QueueProducer
+    {
+        public static void Publish(IModel channel)
+        {
+            channel.QueueDeclare("batch22.queuel",
+                durable:true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+            var count = 10;
+            var message = new
+            {
+                Name = "Producer", Message = $"from Producer:{count } "
+            };
+            var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+            channel.BasicPublish("", "batch22.queuel", null, body);
+        }
+    }
+}
